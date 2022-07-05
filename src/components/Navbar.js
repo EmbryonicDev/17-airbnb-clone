eed41957a0542a4dd3e527579207a4eeca3c688a
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-05 17:08:54 +0200

    Code out Navbar & style

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import airbnbLogo from '../assets/airbnb-logo.png';
+
+export default function Navbar() {
+  return (
+    <div id='header'>
+      <img src={airbnbLogo} alt=""></img>
+    </div>
+  );
+}
