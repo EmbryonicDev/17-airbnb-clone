d6c750f60081ae22196a78957169da1b5efc8429
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-06 10:09:53 +0200

    Prepare files to add Card.js code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Hero } from './components/Hero';
 import Navbar from './components/Navbar';
+import { Hero } from './components/Hero';
+import Card from './components/Card';
 
 export default function App() {
   return (
     <div id='app'>
       <Navbar />
       <Hero />
+      <Card />
 
     </div>
   )
