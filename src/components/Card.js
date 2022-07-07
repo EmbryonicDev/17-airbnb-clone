005f5682f55ed9e436d175d6752b1128d07e1ebd
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-07 11:55:31 +0200

    Ad instructions for next challenge

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,18 @@ import React from "react";
 import katie from '../assets/katie-zaferes.png'
 import star from '../assets/star.png'
 
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
+
 export default function Card() {
   return (
     <div className="card">
