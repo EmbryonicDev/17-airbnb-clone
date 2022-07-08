4844bc26588b4c3965b458293b1b2d61e9b2fee4
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-07-08 16:34:21 +0200

    Modify Cards to accept new props value

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,22 +2,29 @@ import React from "react";
 import star from '../assets/star.png'
 
 export default function Card(props) {
+  console.log(props.item)
+  let badgeText
+  if (props.item.openSpots === 0) {
+    badgeText = 'SOLD OUT'
+  } else if (props.item.location === 'Online') {
+    badgeText = 'ONLINE'
+  }
   return (
     <div className="card">
-      <img src={props.img} alt='' className='cardPic'></img>
-      <div className="imgBtn">SOLD OUT</div>
+      <img src={props.item.coverImg} alt='' className='cardPic'></img>
+      {badgeText && <div className="imgBtn">{badgeText}</div>}
       <div className="cardText">
         <div className="rating">
           <img src={star} alt='' className="star"></img>
-          <span> {props.rating}</span>
-          <span className="greyText"> ({props.reviewCount})</span>
-          <span className="greyText"> {props.location}</span>
+          <span> {props.item.stats.rating}</span>
+          <span className="greyText"> ({props.item.stats.reviewCount})</span>
+          <span className="greyText"> {props.item.location}</span>
         </div>
         <div className="title">
-          {props.title}
+          {props.item.title}
         </div>
         <div className="price">
-          <span>From ${props.price}</span>
+          <span>From ${props.item.price}</span>
           <span> / person</span>
         </div>
       </div>
